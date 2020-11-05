33887e988810bc791c465a6ea87c60a006a4c058
Ivan Tsonev <i_tsonev@abv.bg>
2020-11-05 13:06:55 +0200

    db

---
+++ Dump20201105/borovets-db_menu.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: borovets-db
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (3,'Бурата с пресни зелени и червени чушки','',9.8,1),(4,'Салата с киноа, авокадо, домат конкасе и краставици',NULL,8.2,1),(5,'Листна салата с овче сирене',NULL,7.2,1),(6,'Домати с моцарела,печени чушки и домашно песто',NULL,6.5,1),(7,'Цезар-хрупкаво пиле',NULL,8.4,1),(8,'Цезар със скариди',NULL,9.4,1),(9,'Салата Боровец',NULL,6,1),(10,'Салата табуле',NULL,6,1),(11,'Хумус',' ',6,2),(12,'Телешки език в Хойсин',NULL,9.4,2),(13,'Гъши дроб с пюре от ябълки',NULL,16,2),(14,'Хрупкави панирани ушички със шрирача сос',NULL,8.2,2),(15,'Горски гъби със спанак',NULL,6.2,2),(16,'Пукани белени люти чушки в марината',NULL,4,2),(17,'Скариди в темпура',NULL,11,3),(18,'Калмари на плоча',NULL,12,3),(19,'Пържен сом',NULL,10.8,3),(20,'Октопод с тарама хайвер',NULL,28,3),(21,'Сьомга филе бавно готвени на фурна',NULL,16.8,3),(22,'Свински врат',NULL,10,4),(23,'Телешки стек',NULL,20,4),(24,'Пилешко филе 66 градуса Целзий',NULL,8.8,4),(25,'Телешки кюфтенца с домашно пукани люти чушки',NULL,8.8,4),(26,'Патешко магре с пюре от морков и портокал',NULL,13.8,5),(27,'Ризото пиле и зеленчуци,пармезан слайс',NULL,7.2,5),(28,'Свински гърди бавно готвени с барбекю сос',NULL,12.8,5),(29,'Черен ориз с морски дарове и кокосово мляко',NULL,8.9,5),(30,'Кашкавал Ревански с пюре от червена леща и къри',NULL,7.8,5),(31,'Печен картоф с заквасена сметана,бекон и копър',NULL,3,6),(32,'Микс салата с френски дресинг',NULL,3.5,6),(33,'Пресни пържени картофи',NULL,3.3,6),(34,'Хрупкави грил зеленчуци с билкова марината',NULL,3,6),(35,'Колсло',NULL,2.5,6),(36,'Пепър сос',NULL,2,6),(37,'Гъбен сос',NULL,2,6),(38,'Домашен сос барбекю',NULL,2,6),(39,'Бургер с Бриош питка',NULL,9.2,7),(40,'Хрупкави пилешки филенца',NULL,6.8,7),(41,'Спагети Болонезе',NULL,6.9,7),(42,'Фиш и чипс-сьомга в темпура',NULL,9.2,7),(43,'Франзела',NULL,2.5,8),(44,'Ръчна питка с билки и масло',NULL,2.2,8),(45,'Бисквитена торта с домашен яйчен крем',NULL,4,9),(46,'Сочно шоколадово брауни',NULL,4.5,9),(47,'Тирамису',' ',5,9);
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-05 13:06:10
